82950c497a2d8c082fed48d53d1b149f4bde92a1
Hasnain <hasnainayub689@gmail.com>
2024-02-28 21:04:44 +0400

    update commit

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,18 +3,29 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 
 function Navigation() {
     return (
         <>
             <AppBar position="fixed">
-                <Toolbar className=" flex h-20 justify-between ml-[240px] ">
+                <Toolbar className=" flex h-20 justify-between lg:ml-[240px] md:ml-5 sm:ml-4 ">
                     <IconButton
                         color="inherit"
                         sx={{ display: { xl: 'none' } }}
+                        style={{ outline: 'none' }}
                         edge="start"
+                        onClick={() => {}}
+                        className=" mr-2 sm:ml-0 sm:flex-wrap "
                     >
                         <MenuIcon />
+                    </IconButton>
+                    <IconButton
+                        color="interit"
+                        sx={{ ml: 1 }}
+                        onClick={() => {}}
+                    >
+                        <Brightness4Icon />
                     </IconButton>
                 </Toolbar>
             </AppBar>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+import { dark } from '@mui/material/styles/createPalette';
+
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class',
     theme: {
         extend: {},
     },
